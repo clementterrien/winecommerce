e81cd94a4da8cc25dfebc57420b917c37c9a02e4
Clément Terrien <cterrien@sibylone.com>
2024-05-24 11:58:00 +0200

    creating homepage and navbar

---
+++ assets/js/navbar.js
@@ -0,0 +1,64 @@
+window.onload = function () {
+    var myaccount = $('#myaccount-container');
+        myaccount.mouseenter(function () {
+            myaccount.addClass('hover');
+            $('#account-box').show();
+        });
+        myaccount.mouseleave(function () {
+            myaccount.removeClass('hover');
+            $('#account-box').hide();
+        });
+
+    var boxButtons = $(".box-buttons-button");
+            boxButtons.mouseenter(function () {
+            $(this).addClass('hover');
+        });
+        boxButtons.mouseleave(function () {
+            $(this).removeClass('hover');
+        });
+
+    var newsletter  = $('#newsletter-container');
+        newsletter.mouseover(function () {
+            newsletter.addClass('hover');
+            $('#newsletter-box').show();
+        });
+        newsletter.mouseout(function () {
+            newsletter.removeClass('hover');
+            $('#newsletter-box').hide();
+        })
+
+    var favorite = $('#favorite-container');
+        favorite.mouseover(function () {
+            favorite.addClass('hover');
+        });
+        favorite.mouseout(function () {
+            favorite.removeClass('hover');
+        })
+
+    var cart = $('#cart-container');
+        cart.mouseover(function () {
+            cart.addClass('hover');
+            $('#cart-box').show();
+        });
+        cart.mouseout(function () {
+            cart.removeClass('hover');
+            $('#cart-box').hide();
+        })
+
+    var wineButtons = $('.nav-element-wine-button');
+    wineButtons.mouseenter(function () {
+        $(this).addClass('hover');
+    });
+    wineButtons.mouseleave(function () {
+        $(this).removeClass('hover');
+    });
+
+    var reducedNavButtons = $('.reduced-nav-btn');
+    reducedNavButtons.mouseenter(function () {
+        $(this).addClass('hover');
+    });
+    reducedNavButtons.mouseleave(function () {
+        $(this).removeClass('hover');
+    });
+
+}
---
+++ tests/Controller/HomeControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/home');
+
+        $this->assertResponseIsSuccessful();
+        // $this->assertSelectorTextContains('h1', 'Hello, World!');
+    }
+}
